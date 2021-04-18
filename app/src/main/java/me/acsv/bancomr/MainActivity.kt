5d9a945e0c558f2195442a94640b224546bb5336
Antonio Carlos Silva Vasconcelos <a.silva.vasconcelos@avanade.com>
2021-04-18 16:08:22 -0300

    Initial structure

---
+++ app/src/main/java/me/acsv/bancomr/MainActivity.kt
@@ -0,0 +1,11 @@
+package me.acsv.bancomr
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
